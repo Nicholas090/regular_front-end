cc5063e46dfc63fb2392a791b04f5733f71aeb44
vadik <flamingoar15@icloud.com>
2023-04-02 21:31:55 +0300

    add header styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Main from "./pages/Main";
 
 function App() {
   return (
    <Routes>
+       <Route path="/" element={<Main />} />
        <Route path="/login" element={<Login />} />
        <Route path="/register" element={<Registration />} />
        <Route path="/dashboard" element={<Dashboard />} />
